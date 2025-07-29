b99b6d8668c7cd17badd317ae20885d609939787
khushboo kumari <ksingh200601@gmail.com>
2025-07-29 10:31:46 +0530

    updated server.ts

--- server.ts
+++ server.ts
@@ -1,6 +1,5 @@
-import express from 'express';
-import dotenv from 'dotenv';
-
+import express from "express"
+import dotenv from "dotenv"
 dotenv.config();
 const app = express();
 const PORT=5000;
