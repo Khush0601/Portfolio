3a285c45922f3fac681e3706554f5d514ec44a1f
khushboo kumari <ksingh200601@gmail.com>
2025-08-01 01:38:42 +0530

    added createSkillsDetails

---
+++ controllers/skillController.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from "express";
+import SkillModel from "../models/skillModel"
+
+export const createSkillsDetails = async (req: Request, res: Response): Promise<void> => {
+  try{
+   const skillsData=req.body;
+  
+   const saveSkillsData=await SkillModel.create(skillsData);
+   res.status(201).json({
+    message:"about details created Successfully",
+    data:saveSkillsData,
+   })
+  }
+  catch(error){
+   console.error("Error creating skills details:", error);
+    res.status(500).json({
+      message: "Something went wrong",
+      error,
+    });
+  }
+};
