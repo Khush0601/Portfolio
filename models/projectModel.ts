5daa63c6c808741e353bb461219fa608952ed716
khushboo kumari <ksingh200601@gmail.com>
2025-08-01 12:12:44 +0530

    added projectModel

---
+++ models/projectModel.ts
@@ -0,0 +1,29 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+export interface ProjectDocument extends Document {
+  title: string;
+  description: string;
+  techStack: string[];
+  features: string[];
+  clientRepo?: string;
+  serverRepo?: string;
+  demoVideo?: string;
+  liveLink: string;
+  projectImage: string;
+  date?: string;
+}
+
+const projectSchema: Schema<ProjectDocument> = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  techStack: [{ type: String, required: true }],
+  features: [{ type: String, required: true }],
+  clientRepo: { type: String },
+  serverRepo: { type: String },
+  demoVideo: { type: String },
+  liveLink: { type: String, required: true },
+  projectImage: { type: String, required: true },
+  date: { type: String },
+});
+
+export default mongoose.model<ProjectDocument>("projectDetails", projectSchema);
